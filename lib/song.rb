ee2f43368d62dab8c1c0cd5a2eca40c582e36294
Joshua Sanders <ninjosh2@gmail.com>
2019-12-20 08:58:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,9 @@ class Song
     @artist = artist
     @genre = genre 
     @@count += 1 
-    @@genres << genre  
     @@artists << artist 
+    @@genres << genre  
+    
   end 
   
   def self.count 
