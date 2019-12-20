5a0abe9e8750511861d544ce43ea0f84980c9c32
Joshua Sanders <ninjosh2@gmail.com>
2019-12-20 08:57:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,9 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  @@genre = []
   @@artist = []  
+  @@genre = []
+  
   
   def initialize(song_name, artist, genre) 
     @name = song_name
