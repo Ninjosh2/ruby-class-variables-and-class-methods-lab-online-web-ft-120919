f921fb08c558280fd47f4a315018f01c931d1d9f
Joshua Sanders <ninjosh2@gmail.com>
2019-12-20 08:57:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   @@genre = []
   
   
-  def initialize(song_name, artist, genre) 
+  def initialize(song_name,artist,genre) 
     @name = song_name
     @artist = artist
     @genre = genre 
